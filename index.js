d6fe060cb860cc79e751af152e1ac2783dfc0e19
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-04-11 00:20:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData (name, email) {
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+
+
+    }
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(obj){
+        document.body.innerHTML = obj.id
+    } )
+    .catch(function(error) {
+        document.body.innerHTML = error['message']
+    } )
+}
+
+
+
